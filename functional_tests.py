6f74081780ea012935d5705b81b9021458321979
Robert Percival <robert@mactechs.com>
2016-04-18 16:04:13 -0700

    Added test_add_then_remove_all

--- functional_tests.py
+++ functional_tests.py
@@ -12,5 +12,11 @@ class TestFinderSidebar(unittest.TestCase):
 		for uri in self.pre_items:
 			self.finder.add(uri)
 
+	def test_add_then_remove_all(self):
+		self.finder.add("/tmp")
+		assert 'tmp' in self.finder.favorites.keys()
+		self.finder.removeAll()
+		assert not self.finder.favorites
+
 if __name__ == '__main__':
     unittest.main()
